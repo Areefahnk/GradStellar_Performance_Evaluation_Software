97b5486744016f984040123476c56c56940f2a45
koneru5822 <63387275+koneru5822@users.noreply.github.com>
2023-01-24 22:23:58 +0530

    updated configure_db.py
    imported pandas

--- configure_db.py
+++ configure_db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 con = sqlite3.connect('gradstellar_database.db')
 con.execute("create table performance(g_id int primary key,name text not null,mail_id text not null,day_wise_score float not null,python_project_score float not null,aiml_project_score float not null,interact_score float not null,overall_score float not null,grade text not null,qualify_for_Excellence_Certificate text not null)")
 con.commit()
