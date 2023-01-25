5e00055e0852a9c1e900afca25f3dee83b3a8fb0
Prince Vijay Santhosh Kongara <95931782+Prince-Vijay-Santhosh@users.noreply.github.com>
2023-01-25 21:56:14 +0530

    Update evaluation.py

--- Main_App/Performance_Evaluation_Pack/evaluation.py
+++ Main_App/Performance_Evaluation_Pack/evaluation.py
@@ -13,10 +13,8 @@ def calc_grade(report,score):
         report["grade"]='Very Good'
     elif score>=60:
         report["grade"] = 'Good'
-    elif score>30:
-        report["grade"] = 'Average'
     else:
-        report["grade"]='Poor'
+        report["grade"]='Average'
 
 def get_excellence_certificate(report,grade):
     if grade == 'Excellent':
