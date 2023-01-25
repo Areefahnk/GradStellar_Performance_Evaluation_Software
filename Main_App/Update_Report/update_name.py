882454284d55d6d37435f86c632e96d20e3e021c
Prince Vijay Santhosh Kongara <95931782+Prince-Vijay-Santhosh@users.noreply.github.com>
2023-01-25 21:44:41 +0530

    Update update_name.py
    made changes in line 7 by writing a queiry

--- Main_App/Update_Report/update_name.py
+++ Main_App/Update_Report/update_name.py
@@ -4,6 +4,7 @@ conn = sqlite3.connect('gradstellar_database.db')
 
 
 def update(id,name_new):
-    conn.execute("")
+    conn.execute("update Partcipants set name='"+name+" 'where g_id='"+str(g_id)+"'")
     conn.commit()
     print("Successully updated")
+ 
